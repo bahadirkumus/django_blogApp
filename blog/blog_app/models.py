2dbc0e328430425afca0b519a099ed5b7f804e28
bahadirkumus <bahadirkumus@gmail.com>
2024-05-11 18:21:29 +0300

    pagination

--- blog/blog_app/models.py
+++ blog/blog_app/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 
 class PublishedManager(models.Manager):
@@ -13,7 +14,10 @@ class Post(models.Model):
         PUBLISHED = "PB", "Published"
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(
+        max_length=250,
+        unique_for_date='publish'
+        )
     body = models.TextField()
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,
@@ -40,3 +44,11 @@ class Post(models.Model):
     
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse('blog_app:post_detail', args=[
+            self.publish.year,
+            self.publish.month,
+            self.publish.day,
+            self.slug
+        ])
--- blog/blog_app/views.py
+++ blog/blog_app/views.py
@@ -1,16 +1,30 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.http import Http404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        # if page_number is out of range get last page of results
+        posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        # if page_number is not an integer get the first page
+        posts = paginator.page(1)
     return render(request, 'blog_app/post/list.html', {'posts': posts})
 
-def post_detail(request, id):
+def post_detail(request, year, month, day, post):
     post = get_object_or_404(
         Post,
-        id=id,
-        status=Post.Status.PUBLISHED
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day,
+        status=Post.Status.PUBLISHED,
     )
     return render(request, 'blog_app/post/detail.html', {'post': post})
 
