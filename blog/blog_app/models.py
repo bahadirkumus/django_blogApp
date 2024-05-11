b2ffef5ea4fbb06d973b5f5f879d15798e8a8675
bahadirkumus <bahadirkumus@gmail.com>
2024-05-11 04:41:17 +0300

    initial

---
+++ blog/blog_app/models.py
@@ -0,0 +1,42 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status = Post.Status.PUBLISHED)
+
+class Post(models.Model):
+
+    class Status(models.TextChoices):
+        DRAFT = "DF", "Draft",
+        PUBLISHED = "PB", "Published"
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    body = models.TextField()
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='blog_posts'
+    )
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=2,
+        choices=Status.choices,
+        default=Status.DRAFT
+    )
+    objects = models.Manager() # The default manager.
+    published = PublishedManager() # Our custom manager
+
+    class Meta:
+        ordering = ['-publish'] # to indicate descending order by using a hyphen before the field name
+
+    indexes = [
+        models.Index(fields=['-publish']),
+    ]
+    
+    def __str__(self):
+        return self.title
